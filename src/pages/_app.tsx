605eb417d91485840ab137ad089fa00eccc0ba9e
wato787 <wato787@icloud.com>
2023-06-26 00:09:58 +0900

    redux mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { configureStore } from '@reduxjs/toolkit';
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const store = configureStore({
+    reducer: {
+      // counter: counterReducer,
+    },
+  });
+
+  const theme = createTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </Provider>
+    </ThemeProvider>
+  );
 }
