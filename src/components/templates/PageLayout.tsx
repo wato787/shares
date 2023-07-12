7cbd62910eff613e48942f97e16fbb7ea1d130c1
wato787 <wato787@icloud.com>
2023-07-12 15:07:48 +0900

    ローディング

--- src/components/templates/PageLayout.tsx
+++ src/components/templates/PageLayout.tsx
@@ -30,6 +30,7 @@ import { auth, db } from '../../../firebase';
 import { setGroupId } from '@/slice/groupIdSlice';
 import { doc, getDoc } from 'firebase/firestore';
 import { useAuthContext } from '@/feature/auth/AuthProvider';
+import LoadingScreen from './LoadingScreen';
 
 interface Props {
   children: ReactElement;
@@ -40,6 +41,7 @@ const PageLayout = (props: Props) => {
   const dispatch = useDispatch();
   const open = useSelector((state: RootState) => state.drawer.open);
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const { userId } = useSelector((state: RootState) => state.userId);
   const { user } = useAuthContext();
   //ユーザーメニュー
@@ -67,129 +69,146 @@ const PageLayout = (props: Props) => {
       const userDocRef = doc(db, 'users', userId);
       const userDocSnap = await getDoc(userDocRef);
       dispatch(setGroupId(userDocSnap.data()?.groupId));
+      setIsLoading(true);
     })();
-  }, [userId, dispatch]);
+  }, []);
 
   return (
-    <div className='flex w-full h-full'>
-      <aside
-        className={classNames(
-          'w-1/5  bg-secondary h-screen relative flex flex-col justify-between',
-          !open && 'w-16'
-        )}
-      >
-        <div>
-          <div className='flex items-center justify-around border-b h-16 z-10'>
-            <Image
-              src='/../public/largelogo.png'
-              width={40}
-              height={20}
-              alt='Shares'
-            />
-            {open && (
-              <>
-                <span className='text-3xl font-bold font-shares text-primary'>
-                  Shares
-                </span>
-                <Image
-                  src='/../public/largelogo.png'
-                  width={40}
-                  height={20}
-                  alt='Shares'
-                />
-              </>
-            )}
-          </div>
-          <Link
-            className={classNames(
-              'w-full px-4 py-5 flex items-center border-b relative hover:bg-white',
-              !open && 'justify-center',
-              props.current === 'dashboad' || !props.current ? 'bg-white' : ''
-            )}
-            href={{ pathname: '/', query: { current: 'dashboad' } }}
-          >
-            <DashboardIcon color='primary' />
-            {open && (
-              <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-                <span className='font-bold text-gray-500'>ダッシュボード</span>
+    <>
+      {isLoading ? (
+        <>
+          <div className='flex w-full h-full'>
+            <aside
+              className={classNames(
+                'w-1/5  bg-secondary h-screen relative flex flex-col justify-between',
+                !open && 'w-16'
+              )}
+            >
+              <div>
+                <div className='flex items-center justify-around border-b h-16 z-10'>
+                  <Image
+                    src='/../public/largelogo.png'
+                    width={40}
+                    height={20}
+                    alt='Shares'
+                  />
+                  {open && (
+                    <>
+                      <span className='text-3xl font-bold font-shares text-primary'>
+                        Shares
+                      </span>
+                      <Image
+                        src='/../public/largelogo.png'
+                        width={40}
+                        height={20}
+                        alt='Shares'
+                      />
+                    </>
+                  )}
+                </div>
+                <Link
+                  className={classNames(
+                    'w-full px-4 py-5 flex items-center border-b relative hover:bg-white',
+                    !open && 'justify-center',
+                    props.current === 'dashboad' || !props.current
+                      ? 'bg-white'
+                      : ''
+                  )}
+                  href={{ pathname: '/', query: { current: 'dashboad' } }}
+                >
+                  <DashboardIcon color='primary' />
+                  {open && (
+                    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                      <span className='font-bold text-gray-500'>
+                        ダッシュボード
+                      </span>
+                    </div>
+                  )}
+                </Link>
+                <Link
+                  className={classNames(
+                    'w-full px-4 py-5 flex items-center border-b relative hover:bg-white',
+                    !open && 'justify-center',
+                    props.current === 'graph' ? 'bg-white' : ''
+                  )}
+                  href={{ pathname: '/graph', query: { current: 'graph' } }}
+                >
+                  <ShowChartIcon color='primary' />
+                  {open && (
+                    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                      <span className='font-bold text-gray-500'>グラフ</span>
+                    </div>
+                  )}
+                </Link>
               </div>
-            )}
-          </Link>
-          <Link
-            className={classNames(
-              'w-full px-4 py-5 flex items-center border-b relative hover:bg-white',
-              !open && 'justify-center',
-              props.current === 'graph' ? 'bg-white' : ''
-            )}
-            href={{ pathname: '/graph', query: { current: 'graph' } }}
-          >
-            <ShowChartIcon color='primary' />
-            {open && (
-              <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-                <span className='font-bold text-gray-500'>グラフ</span>
-              </div>
-            )}
-          </Link>
-        </div>
 
-        <div
-          className={classNames(
-            'flex justify-end p-4 border-t',
-            !open && 'justify-center'
-          )}
-        >
-          {open ? (
-            <IconButton size='small' onClick={handleToggleDrawer}>
-              <ArrowBackIosNewIcon />
-            </IconButton>
-          ) : (
-            <IconButton size='small' onClick={handleToggleDrawer}>
-              <ArrowForwardIosIcon />
-            </IconButton>
-          )}
+              <div
+                className={classNames(
+                  'flex justify-end p-4 border-t',
+                  !open && 'justify-center'
+                )}
+              >
+                {open ? (
+                  <IconButton size='small' onClick={handleToggleDrawer}>
+                    <ArrowBackIosNewIcon />
+                  </IconButton>
+                ) : (
+                  <IconButton size='small' onClick={handleToggleDrawer}>
+                    <ArrowForwardIosIcon />
+                  </IconButton>
+                )}
+              </div>
+            </aside>
+            <div className='flex flex-col w-full'>
+              <header className='flex items-center justify-end h-16 px-6 py-1 bg-secondary border-b w-full'>
+                <Tooltip title='プロフィール'>
+                  <IconButton onClick={handleOpenUserMenu}>
+                    <Avatar
+                      sx={{ bgcolor: 'lightblue' }}
+                      aria-label='recipe'
+                      src={user?.photoURL as string}
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id='menu-appbar'
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  <Box>
+                    <MenuItem>
+                      <SettingsInputComponent sx={{ mr: 2 }} /> ユーザー設定
+                    </MenuItem>
+                    <MenuItem onClick={handleLogout}>
+                      <LogoutIcon sx={{ mr: 2 }} />
+                      ログアウト
+                    </MenuItem>
+                  </Box>
+                </Menu>
+              </header>
+              <div className='m-5'>
+                {cloneElement(props.children, { open })}
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div>
+          <LoadingScreen />
         </div>
-      </aside>
-      <div className='flex flex-col w-full'>
-        <header className='flex items-center justify-end h-16 px-6 py-1 bg-secondary border-b w-full'>
-          <Tooltip title='プロフィール'>
-            <IconButton onClick={handleOpenUserMenu}>
-              <Avatar
-                sx={{ bgcolor: 'lightblue' }}
-                aria-label='recipe'
-                src={user?.photoURL as string}
-              />
-            </IconButton>
-          </Tooltip>
-          <Menu
-            sx={{ mt: '45px' }}
-            id='menu-appbar'
-            anchorEl={anchorElUser}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={Boolean(anchorElUser)}
-            onClose={handleCloseUserMenu}
-          >
-            <Box>
-              <MenuItem>
-                <SettingsInputComponent sx={{ mr: 2 }} /> ユーザー設定
-              </MenuItem>
-              <MenuItem onClick={handleLogout}>
-                <LogoutIcon sx={{ mr: 2 }} />
-                ログアウト
-              </MenuItem>
-            </Box>
-          </Menu>
-        </header>
-        <div className='m-5'>{cloneElement(props.children, { open })}</div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
