d88b081ef630a00861514c9e471c9111f53e60bf
ck <shibaduke4@gmail.com>
2023-07-15 15:55:07 +0900

    フォーム

--- src/components/organisms/card/InputCard.tsx
+++ src/components/organisms/card/InputCard.tsx
@@ -1,4 +1,5 @@
-import { useAuthContext } from '@/feature/auth/authProvider';
+import { useAuthContext } from '@/feature/auth/AuthProvider';
+import { RootState } from '@/store';
 import {
   Avatar,
   Button,
@@ -7,10 +8,31 @@ import {
   Select,
   TextField,
 } from '@mui/material';
+import { addDoc, collection } from 'firebase/firestore';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { db } from '../../../../firebase';
 
 const InputCard = () => {
   const { user } = useAuthContext();
+  const [selectCostType, setSelectCostType] = useState('');
+  const [amount, setAmount] = useState('');
+  const { groupId } = useSelector((state: RootState) => state.groupId);
 
+  const addCost = async () => {
+    if (!groupId) return;
+    const groupRef = collection(db, 'group', groupId, 'cost');
+    await addDoc(groupRef, {
+      costType: selectCostType,
+      cost: parseInt(amount),
+      date: new Date(),
+      inputUserId: user?.uid,
+      inputUserName: user?.displayName,
+      inputUserPhotoUrl: user?.photoURL,
+    });
+    setAmount('');
+    setSelectCostType('');
+  };
 
   return (
     <div className='w-1/3 '>
@@ -26,12 +48,15 @@ const InputCard = () => {
             />
             <span>{user?.displayName}</span>
           </div>
-          <form className='flex flex-col  w-full'>
+          <form onSubmit={addCost} className='flex flex-col  w-full'>
             <span className='text-gray-500 font-bold text-xs mb-2'>
               出費項目
             </span>
             <div className='space-y-8'>
-              <Select fullWidth>
+              <Select
+                fullWidth
+                onChange={(e) => setSelectCostType(e.target.value as string)}
+              >
                 <MenuItem value='家賃'>家賃</MenuItem>
                 <MenuItem value='雑費'>雑費</MenuItem>
                 <MenuItem value='水道代'>水道代</MenuItem>
@@ -44,13 +69,15 @@ const InputCard = () => {
                 id='input-with-sx'
                 label='金額'
                 fullWidth
+                value={amount}
+                onChange={(e) => setAmount(e.target.value as string)}
               />
               <Button
                 variant='outlined'
                 fullWidth
                 sx={{ height: 50 }}
                 color='primary'
-
+                onClick={addCost}
               >
                 送信
               </Button>
