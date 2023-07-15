c166d8943b8ce3285a25106917c8b27ff0473188
wato787 <wato787@icloud.com>
2023-07-15 15:03:34 +0900

    lkjnhbg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import PageLayout from '@/components/templates/PageLayout';
 import { Current, CurrentPageType } from '@/types/type';
 import { Button, TextField } from '@mui/material';
-import { addDoc, collection, doc, getDoc, setDoc } from 'firebase/firestore';
+import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
 import { db } from '../../firebase';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -45,20 +45,12 @@ export default function Home({ current }: Current) {
     showSnackbar('グループを作成しました', 'success');
   };
 
-  // 再読み込み時にグループID取得
-  const handleReload = async (): Promise<void> => {
-    if (!userId) return;
-    const userDocRef = doc(db, 'users', userId);
-    const userDocSnap = await getDoc(userDocRef);
-    dispatch(setGroupId(userDocSnap.data()?.groupId));
-  };
-
   // 	TODO: グループ加入 ユーザーにgroupId格納
 
   return (
     <PageLayout current={current}>
       <>
-        {/* TODO:groupIDで分岐ではなくデータ取得のローディングで分岐する */}
+        {/* TODO:データ取得のローディングで分岐する */}
         {groupId ? (
           <div
             className={classNames(
@@ -81,7 +73,6 @@ export default function Home({ current }: Current) {
             {/* <Button>加入</Button> */}
             <TextField value={name} onChange={(e) => setName(e.target.value)} />
             <Button onClick={handleCreateGroup}>作成</Button>
-            <Button onClick={handleReload}>再読み込み</Button>
             {/* <p>{groupId}</p> */}
           </>
         )}
--- src/types/type.ts
+++ src/types/type.ts
@@ -12,5 +12,5 @@ export type Current = {
 export type CostData = {
   date: string; //2021-01-01
   cost: number; //100000
-  costType: string; //housing,utilityなど家賃、雑費などを英語で
+  costType: string; //utilityなど家賃、雑費などを英語で
 };
