156866d9efaced3c37dfc8e7a9cce7b18a4747a1
ck <shibaduke4@gmail.com>
2023-07-17 23:23:39 +0900

    group表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,8 @@ import TotalCard from '@/components/organisms/card/TotalCard';
 import IndividualCard from '@/components/organisms/card/IndividualCard';
 import ExpensesCard from '@/components/organisms/card/ExpensesCard';
 import { useAuthContext } from '@/feature/auth/AuthProvider';
+import GroupCreateCard from '@/components/organisms/card/GroupcreateCard';
+import GroupJoinCard from '@/components/organisms/card/GroupJoinCard';
 
 export default function Home({ current }: Current) {
   const [name, setName] = useState('');
@@ -21,7 +23,6 @@ export default function Home({ current }: Current) {
   const [joinPositon, setJoinPosition] = useState('');
   const { userId } = useSelector((state: RootState) => state.userId);
   const { groupId } = useSelector((state: RootState) => state.groupId);
-
   const [joinId, setJoinId] = useState('');
   const dispatch = useDispatch();
   const { showSnackbar } = useSnackbar();
@@ -86,7 +87,7 @@ export default function Home({ current }: Current) {
     <PageLayout current={current} grayBg>
       <>
         {/* TODO:データ取得のローディングで分岐する */}
-        {!groupId ? (
+        {groupId ? (
           <div className={classNames('p-5 w-full flex flex-col gap-y-10')}>
             <ExpensesCard />
 
@@ -104,39 +105,16 @@ export default function Home({ current }: Current) {
           </div>
         ) : (
           <>
-            <div className='p-5 flex  items-center justify-center gap-y-5 w-full'>
-              <div className='flex flex-col w-full p-5'>
-                <span className=' text-center'>グループIDに参加する</span>
-                <TextField
-                  value={joinId}
-                  onChange={(e) => setJoinId(e.target.value)}
-                  label='グループID'
-                />
-                <TextField
-                  value={joinPositon}
-                  onChange={(e) => setJoinPosition(e.target.value)}
-                  label='役割'
-                />
-                <Button onClick={handleJoinGroup}>加入</Button>
-              </div>
-
-              <div className='flex flex-col w-full p-5'>
-                <span className='text-center'>グループを作成する</span>
-                <TextField
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  label='id'
-                />
-
-                <TextField
-                  value={position}
-                  onChange={(e) => setPosition(e.target.value)}
-                  label='役割'
-                />
-                <Button onClick={handleCreateGroup}>作成</Button>
-                {/* <p>{groupId}</p> */}
-              </div>
-            </div>
+        <div className='p-5'>
+        <div className='flex  w-full gap-x-5'>
+          <div className='w-1/2 '>
+            <GroupCreateCard />
+          </div>
+          <div className='w-1/2'>
+            <GroupJoinCard />
+          </div>
+        </div>
+      </div>
           </>
         )}
       </>
--- src/pages/setting.tsx
+++ src/pages/setting.tsx
@@ -1,23 +1,47 @@
+import GroupJoinCard from '@/components/organisms/card/GroupJoinCard';
+import GroupCreateCard from '@/components/organisms/card/GroupcreateCard';
 import InputCard from '@/components/organisms/card/InputCard';
 import PageLayout from '@/components/templates/PageLayout';
+import { RootState } from '@/store';
 import { Current, CurrentPageType } from '@/types/type';
 
 import React, { ReactElement } from 'react';
+import { useSelector } from 'react-redux';
 
 const Setting = ({ current }: Current): ReactElement => {
+  const { groupId } = useSelector((state: RootState) => state.groupId);
   return (
-    <PageLayout current={current} grayBg>
-      <div className='flex flex-col w-full p-5'>
-        <div className='flex  w-full gap-x-5'>
-          <div className='w-1/2'>
-            <InputCard />
-          </div>
-          <div className='w-1/2'>
-            <InputCard />
-          </div>
-        </div>
-      </div>
-    </PageLayout>
+    <>
+      <PageLayout current={current} grayBg>
+        {groupId ? (
+          <>
+            <div className='flex flex-col w-full p-5'>
+              <div className='flex  w-full gap-x-5'>
+                <div className='w-1/2'>
+                  <InputCard />
+                </div>
+                <div className='w-1/2'>
+                  <InputCard />
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className=' p-5'>
+              <div className='flex  w-full gap-x-5'>
+                <div className='w-1/2 '>
+                  <GroupCreateCard />
+                </div>
+                <div className='w-1/2'>
+                  <GroupJoinCard />
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </PageLayout>
+    </>
   );
 };
 
