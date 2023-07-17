f77ef0872879324db19d6436af3c150849b054cf
wato787 <wato787@icloud.com>
2023-07-17 16:49:34 +0900

    sedf

--- src/pages/setting.tsx
+++ src/pages/setting.tsx
@@ -8,10 +8,24 @@ import React, { ReactElement } from 'react';
 const Setting = ({ current }: Current): ReactElement => {
   return (
     <PageLayout current={current} grayBg>
-      <div className='flex flex-col w-full p-5'>
+      <div className='flex flex-col w-full p-5 gap-y-8'>
         <div className='flex  w-full gap-x-10'>
-          <div className='w-1/3'>
+          <div className='w-1/3 '>
             <RentCard />
+          </div>
+          <div className='w-1/3'>
+            <InviteCard />
+          </div>
+          <div className='w-1/3'>
+            <InviteCard />
+          </div>
+        </div>
+        <div className='flex  w-full gap-x-10'>
+          <div className='w-1/3 '>
+            <RentCard />
+          </div>
+          <div className='w-1/3'>
+            <InviteCard />
           </div>
           <div className='w-1/3'>
             <InviteCard />
