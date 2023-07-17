d3844a2cd3407af0c09630f2c89a6be4cac89f87
wato787 <wato787@icloud.com>
2023-07-17 21:25:48 +0900

    console削除

--- src/components/organisms/card/MemberCard.tsx
+++ src/components/organisms/card/MemberCard.tsx
@@ -4,10 +4,10 @@ import { useSelector } from 'react-redux';
 
 const MemberCard = () => {
   const groupUsers = useSelector((state: RootState) => state.groupUsers);
-  console.log(groupUsers);
+
   return (
     <Card sx={{ height: '100%' }}>
-      <div className='p-5  flex flex-col items-center justify-center gap-y-5'>
+      <div className='p-5  flex flex-col items-center justify-center gap-y-5 overflow-scroll'>
         <span className='font-bold text-xl text-gray-600'>メンバー</span>
         <div className='w-full space-y-4'>
           {groupUsers.map((user) => (
