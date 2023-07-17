0773b43744c1cd89680f726449d27e47257bac1c
wato787 <wato787@icloud.com>
2023-07-17 17:16:49 +0900

    設定追加

--- src/components/templates/PageNavigation.tsx
+++ src/components/templates/PageNavigation.tsx
@@ -6,7 +6,7 @@ import { RootState } from '@/store';
 import { useSelector } from 'react-redux';
 import { Current } from '@/types/type';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import SettingsIcon from '@mui/icons-material/Settings';
 import ShowChartIcon from '@mui/icons-material/ShowChart';
 
 const PageNavigation = ({ current }: Current): ReactElement => {
@@ -72,7 +72,7 @@ const PageNavigation = ({ current }: Current): ReactElement => {
         )}
         href={{ pathname: '/setting', query: { current: 'setting' } }}
       >
-        <ManageAccountsIcon color='primary' />
+        <SettingsIcon color='primary' />
         {open && (
           <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
             <span className='font-bold text-gray-500'>設定 ＆ 管理</span>
