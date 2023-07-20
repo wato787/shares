f29ff596faa25c5100ce65e5116da3cb89363101
wato787 <wato787@icloud.com>
2023-07-20 14:26:09 +0900

    月別詳細リンク追加

--- src/components/templates/PageNavigation.tsx
+++ src/components/templates/PageNavigation.tsx
@@ -8,6 +8,7 @@ import { Current } from '@/types/type';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import SettingsIcon from '@mui/icons-material/Settings';
 import ShowChartIcon from '@mui/icons-material/ShowChart';
+import HistoryIcon from '@mui/icons-material/History';
 
 const PageNavigation = ({ current }: Current): ReactElement => {
   const open = useSelector((state: RootState) => state.drawer.open);
@@ -62,6 +63,21 @@ const PageNavigation = ({ current }: Current): ReactElement => {
         {open && (
           <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
             <span className='font-bold text-gray-500'>グラフ</span>
+          </div>
+        )}
+      </Link>
+      <Link
+        className={classNames(
+          'w-full px-4 py-5 flex items-center border-b relative hover:bg-white',
+          !open && 'justify-center',
+          current === 'detail' ? 'bg-white' : ''
+        )}
+        href={{ pathname: '/detail', query: { current: 'detail' } }}
+      >
+        <HistoryIcon color='primary' />
+        {open && (
+          <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+            <span className='font-bold text-gray-500'>月別の詳細</span>
           </div>
         )}
       </Link>
