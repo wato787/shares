24e1777504e28b2d29e59b2eb786f9c24c049122
wato787 <wato787@icloud.com>
2023-07-20 14:46:52 +0900

    サイドバー調整

--- src/components/templates/PageNavigation.tsx
+++ src/components/templates/PageNavigation.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import Link from 'next/link';
-import React, { ReactElement, useMemo } from 'react';
+import React, { ReactElement } from 'react';
 import Image from 'next/image';
 import { RootState } from '@/store';
 import { useSelector } from 'react-redux';
@@ -10,38 +10,39 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import ShowChartIcon from '@mui/icons-material/ShowChart';
 import HistoryIcon from '@mui/icons-material/History';
 
-const navigationItems = useMemo(
-  () => [
-    {
-      href: { pathname: '/', query: { current: 'dashboard' } },
-      label: 'ダッシュボード',
-      icon: <DashboardIcon color='primary' />,
-    },
-    {
-      href: { pathname: '/graph', query: { current: 'graph' } },
-      label: 'グラフ',
-      icon: <ShowChartIcon color='primary' />,
-    },
-    {
-      href: {
-        pathname: '/detail',
-        query: {
-          year: new Date().getFullYear(),
-          month: new Date().getMonth() + 1,
-          current: 'detail',
-        },
+const navigationItems = [
+  {
+    href: { pathname: '/', query: { current: 'dashboard' } },
+    label: 'ダッシュボード',
+    icon: <DashboardIcon color='primary' />,
+    labelEn: 'dashboard',
+  },
+  {
+    href: { pathname: '/graph', query: { current: 'graph' } },
+    label: 'グラフ',
+    icon: <ShowChartIcon color='primary' />,
+    labelEn: 'graph',
+  },
+  {
+    href: {
+      pathname: '/detail',
+      query: {
+        year: new Date().getFullYear(),
+        month: new Date().getMonth() + 1,
+        current: 'detail',
       },
-      label: '月別の詳細',
-      icon: <HistoryIcon color='primary' />,
     },
-    {
-      href: { pathname: '/setting', query: { current: 'setting' } },
-      label: '設定 ＆ 管理',
-      icon: <SettingsIcon color='primary' />,
-    },
-  ],
-  [] // 依存配列は空にすることで、最初の一度だけメモ化する
-);
+    label: '月別の詳細',
+    icon: <HistoryIcon color='primary' />,
+    labelEn: 'detail',
+  },
+  {
+    href: { pathname: '/setting', query: { current: 'setting' } },
+    label: '設定 ＆ 管理',
+    icon: <SettingsIcon color='primary' />,
+    labelEn: 'setting',
+  },
+];
 
 const PageNavigation = ({ current }: Current): ReactElement => {
   const open = useSelector((state: RootState) => state.drawer.open);
@@ -77,8 +78,11 @@ const PageNavigation = ({ current }: Current): ReactElement => {
           className={classNames(
             'w-full px-4 py-5 flex items-center border-b relative hover:bg-white',
             !open && 'justify-center',
-            current === item.label ||
-              (!current && item.label === 'ダッシュボード')
+            current === item.labelEn ||
+              (!current &&
+                item.labelEn === 'dashboard' &&
+                typeof window !== 'undefined' &&
+                window.location.pathname === '/')
               ? 'bg-white'
               : ''
           )}
