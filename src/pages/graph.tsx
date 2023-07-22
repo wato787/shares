c874a56b73a7e0b9e0f26453c14028f23d079abe
wato787 <wato787@icloud.com>
2023-07-22 17:09:11 +0900

    不要部分削除

--- src/pages/graph.tsx
+++ src/pages/graph.tsx
@@ -1,34 +1,14 @@
 import Chart from '@/components/organisms/Chart';
-import GroupJoinCard from '@/components/organisms/card/GroupJoinCard';
-import GroupCreateCard from '@/components/organisms/card/GroupcreateCard';
 import PageLayout from '@/components/templates/PageLayout';
-import { RootState } from '@/store';
 import { Current, CurrentPageType } from '@/types/type';
 
 import React, { ReactElement } from 'react';
-import { useSelector } from 'react-redux';
 
 const graph = ({ current }: Current): ReactElement => {
-  const { groupId } = useSelector((state: RootState) => state.groupId);
   return (
     <>
-        <PageLayout current={current} grayBg>
-      {groupId ? (
-          <Chart />
-          ) : (
-            <>
-          <div className='p-5 '>
-        <div className='flex  w-full gap-x-5'>
-          <div className='w-1/2 '>
-            <GroupCreateCard />
-          </div>
-          <div className='w-1/2'>
-            <GroupJoinCard />
-          </div>
-        </div>
-      </div>
-        </>
-      )}
+      <PageLayout current={current}>
+        <Chart />
       </PageLayout>
     </>
   );
