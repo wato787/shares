754fd3997ca296a4425fc19153490fa84066fb8e
wato787 <wato787@icloud.com>
2023-07-29 15:08:23 +0900

    enum変更

--- src/utils/CostType.ts
+++ src/utils/CostType.ts
@@ -1,11 +1,12 @@
-// 費用の種類
-export enum CostType {
-  RENT = 'rent', //家賃
-  MISCELLANEOUS = 'miscellaneous', //雑費
-  WATER = 'water', //水道
-  GAS = 'gas', //ガス
-  UTILITIES = 'utilities', //光熱費
-  FOOD = 'food', //食費
-  COMMUNICATION = 'communication', //通信費
-  OTHER = 'other', //その他
-}
+export const CostType = {
+  RENT: 'rent', //家賃
+  MISCELLANEOUS: 'miscellaneous', //雑費
+  WATER: 'water', //水道
+  GAS: 'gas', //ガス
+  UTILITIES: 'utilities', //光熱費
+  FOOD: 'food', //食費
+  COMMUNICATION: 'communication', //通信費
+  OTHER: 'other', //その他
+} as const;
+
+export type CostType = (typeof CostType)[keyof typeof CostType];
