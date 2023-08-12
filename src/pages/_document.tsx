c7e726ea6825fe0642a4803527497372a2bf14c5
wato787 <wato787@icloud.com>
2023-08-12 18:17:12 +0900

    add head key

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,11 @@ export default function Document() {
     <Html lang='en'>
       <Head>
         <title>Shares</title>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta
+          name='viewport'
+          content='initial-scale=1.0, width=device-width'
+          key='title'
+        />
       </Head>
       <body>
         <Main />
