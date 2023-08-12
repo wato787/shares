8e6fec7018b5cfd22d814ab767d14b89a04849b6
wato787 <wato787@icloud.com>
2023-08-12 18:15:58 +0900

    headタグ追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Main, NextScript } from 'next/document';
+import Head from 'next/head';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <title>Shares</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
